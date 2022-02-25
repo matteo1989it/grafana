e21c6ed1b99a9d0938bd7117d3d329394fb0bf5e
matteo1989it <matteodeleo89@gmail.com>
2022-02-25 17:10:14 +0100

    fixed typo in post.sh

--- config/post.sh
+++ config/post.sh
@@ -1,15 +1,5 @@
 ﻿#!/bin/sh
-echo creating datasource
 curl -u admin:admin -X POST -H "Content-Type:application/json" --data-binary "@/etc/config/datasources/datasource.json" http://localhost:3000/api/datasources
-echo "\ndatasource created"
-echo creating dashboard
-curl -u admin:admin -X POST -H "Content-Type:application/json" --data-binary "@/etc/config/daashboards/metrics_kpi.json" http://localhost:3000/api/dashboards/db
-echo "\ndashboard created"
-echo "Press any key to continue"
-while [ true ] ; do
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
+curl -u admin:admin -X POST -H "Content-Type:application/json" --data-binary "@/etc/config/dashboards/metrics_kpi.json" http://localhost:3000/api/dashboards/db
+curl -u admin:admin -X POST -H "Content-Type:application/json" --data-binary "@/etc/config/dashboards/m3trics_intent.json" http://localhost:3000/api/dashboards/db
+printf "dashboards created"
